90905f2f9e1ffad2f9d9db2f72f4ff6b0942f62d
Balrog57 <soliani.marc@gmail.com>
2026-01-12 22:34:34 +0100

    Bump version to 2.0.3 & Fix CI upload pattern to be robust

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ except ImportError:
     logger.error("Impossible d'importer utils.py")
     utils = None
 
-VERSION = "2.0.2"
+VERSION = "2.0.3"
 
 # Configuration du logging
 # Configuration du logging
