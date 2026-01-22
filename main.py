414dc97e40d936238b2fad94b8a2a8d21c4da414
Balrog57 <soliani.marc@gmail.com>
2026-01-22 18:59:18 +0100

    Bump version to 2.0.35

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ except ImportError:
     logger.error("Impossible d'importer utils.py")
     utils = None
 
-VERSION = "2.0.34"
+VERSION = "2.0.35"
 
 # Configuration du logging
 local_app_data = os.getenv('LOCALAPPDATA')
